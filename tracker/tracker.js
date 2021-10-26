f2a6a020f967570e593a60db7b61420d3fa83aef
MatiasC248 <MatiasC248@users.noreply.github.com>
2021-10-26 20:35:42 -0300

    Pruebas sha1, store rudimentario

--- tracker/tracker.js
+++ tracker/tracker.js
@@ -1,17 +1,37 @@
+sha1 = require('js-sha1');
 
-(function crearTracker() {
+var tracker;
+
+function crearTracker() {
   let config = require("./inicialConfig.json");
-  let tracker = {
+  tracker = {
     ant: config.direcciones.ant,
     sig: config.direcciones.sig,
-    diccionario: new Map(),
+    //diccionario: new Map(),
+    diccionario: [],
     id: config.id,
   };
+}
 
-  
-})
+function store(name, size, peers) {
+  //falta almacenar el archivo en el nodo par
+  let hash = sha1(name);
+  let index = parseInt(hash.slice(0,2),16);
+  tracker.diccionario[index] = new Map(Object.entries({
+    hash: hash,
+    name: name,
+    size: size,
+    peers: peers //objeto que contiene los pares que tienen el archivo
+  }));
+}
 
+//test sha1
+//console.log(sha1('ArchivoPrueba.txt'));
+//console.log(sha1('1').slice(0,2));
+//console.log(parseInt(sha1('ArchivoPrueba.txt').slice(0,2),16));
 
-
+crearTracker();
+store('ArchivoPrueba.txt',3,{ host: 'hostprueba', port: 3001 });
+console.log(Object.fromEntries((tracker.diccionario[67]).entries()));
 
 
